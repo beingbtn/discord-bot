7ef47fe023317b72a6b147eec08e98425f85690c
Attituding <84680760+Attituding@users.noreply.github.com>
2022-05-06 23:01:24 -0700

    fix: release client

--- src/utility/database.ts
+++ src/utility/database.ts
@@ -1,10 +1,12 @@
 import { Pool } from 'pg';
 import { Log } from './Log';
 
-const pool = new Pool();
+const pool = new Pool({
+    idleTimeoutMillis: 300_000,
+});
 
 pool.on('error', error => {
-    Log.error(`PostgreSQL Pool Error | Total Clients: ${pool.totalCount} Idle CLients: ${pool.idleCount}`, error.stack);
+    Log.error(`PostgreSQL Pool Error | Total Clients: ${pool.totalCount} Idle Clients: ${pool.idleCount} Waiting Clients: ${pool.waitingCount}`, error.stack);
 });
 
 export class Database {
@@ -15,9 +17,8 @@ export class Database {
             const query = await client.query(input, values);
 
             return query;
-        } catch (error) {
+        } finally {
             client.release();
-            throw error;
         }
     }
 
