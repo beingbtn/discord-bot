e22cccb2b656f119265ce70c2be1e48f90ff4d5e
Attituding <84680760+Attituding@users.noreply.github.com>
2022-06-20 16:14:10 -0700

    refactor: simply test.ts feat: undeploy multiple guilds w/ undeploy.ts

---
+++ src/undeploy.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
+import process from 'node:process';
+import { i18n } from './locales/i18n';
+
+(async () => {
+    try {
+        const rest = new REST({ version: '10' })
+            .setToken(process.env.DISCORD_TOKEN!);
+
+        const guildIDs = JSON.parse(process.env.OWNER_GUILDS!) as string[];
+
+        await Promise.all([
+            rest.put(
+                Routes.applicationCommands(
+                    process.env.CLIENT_ID!,
+                ), {
+                    body: [],
+                },
+            ),
+            ...guildIDs.map((guildID) => rest.put(
+                Routes.applicationGuildCommands(
+                    process.env.CLIENT_ID!,
+                    guildID,
+                ), {
+                    body: [],
+                },
+            )),
+        ]);
+
+        console.log(new i18n().getMessage('commandsDeployTitle'));
+    } catch (error) {
+        console.error(error);
+    }
+})();
