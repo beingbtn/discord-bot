da6b3adb1695390538a7a169c20d50eaf13a74e4
Attituding <84680760+Attituding@users.noreply.github.com>
2022-08-18 13:29:54 -0700

    feat: make Performance better

--- src/structures/Performance.ts
+++ src/structures/Performance.ts
@@ -1,41 +1,46 @@
-import { LimitedCollection } from 'discord.js';
+import { Collection } from 'discord.js';
 import { Options } from '../utility/Options';
+
+/* eslint-disable no-underscore-dangle */
 
 type PerformanceOptions = {
     interval?: number,
     maxDataPoints?: number,
 };
 
-export class Performance<T extends Record<string, unknown>> {
-    public current: T;
+export class Performance {
+    public current: Collection<string, number>;
 
     public interval: number;
 
-    public latest: T | null;
+    public latest: Collection<string, number> | null;
 
-    public readonly dataPoints: LimitedCollection<number, T>;
+    public readonly dataPoints: Collection<string, number>[];
+
+    public readonly maxDataPoints: number;
 
     constructor(options?: PerformanceOptions) {
-        this.current = {} as T;
-        this.dataPoints = new LimitedCollection({
-            maxSize: options?.maxDataPoints ?? Options.performanceMaxDataPoints,
-        });
+        this.current = new Collection();
+        this.dataPoints = [];
         this.interval = options?.interval ?? Options.performanceInterval;
         this.latest = null;
+        this.maxDataPoints = options?.maxDataPoints ?? Options.performanceMaxDataPoints;
     }
 
-    public addData(key: keyof T, value: T[keyof T]) {
-        this.current[key] = value;
+    public addData(key: string) {
+        this.current.set(key, Date.now());
     }
 
     public addDataPoint() {
-        const latestTimestamp = this.dataPoints.firstKey();
+        const latestTimestamp = this.dataPoints[0]?.get('_timestamp');
 
         if (
             latestTimestamp
             && Date.now() > latestTimestamp + this.interval
         ) {
-            this.dataPoints.set(Date.now(), this.current);
+            this.current.set('_timestamp', Date.now());
+            this.dataPoints.push(this.current);
+            this.dataPoints.length = this.maxDataPoints;
         }
 
         this.latest = this.current;
